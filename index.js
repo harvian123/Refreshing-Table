7a38155236e01fbb15798a31ad911a23f0197c0b
Harvian <67323489+harvian123@users.noreply.github.com>
2022-02-11 18:51:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const shuffleArray = require('shuffle-array');
-const chance = require('chance').Chance();
-
-const app = express();
-app.use(express.static("public"));
-
-const data = {
-    headers : ["Nama", "Umur", "Profesi", "Negara"],
-    rows : new Array(10).fill(undefined).map(()=>{
-        return [
-            chance.name(),
-            chance.age(),
-            chance.profession(),
-            chance.country({ full:true })
-        ]
-    })
-}
-
-app.get('/data', (req, res) => {
-    res.json({
-        headers: data.headers,
-        rows: shuffleArray(data.rows),
-        lastUpdated: new Date().toISOString()
-    });
-});
-
-
-
-app.listen(3000, ()=>{
-    console.log("App is Running at Port 3000")
-=======
 const express = require('express');
 const shuffleArray = require('shuffle-array');
 const chance = require('chance').Chance();
